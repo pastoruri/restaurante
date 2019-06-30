d9537bdfc2e7a968fcc6409cdc43660c17ac9f02
pastoruri <40170714+pastoruri@users.noreply.github.com>
2019-06-30 16:40:07 -0500

    Update mesas.h

--- restaurante/mesas.h
+++ restaurante/mesas.h
@@ -9,14 +9,13 @@
 
 class Mesa{
     int mesa_id;
-    Cliente * cliente;
     bool state;
 public:
     Mesa(int id, bool state_) :
     mesa_id{id}, state{state_} {}
     void update(bool state_){state=state_;};
     int getid(){return mesa_id;};
-    void setcliente(Cliente* cliente_){cliente=cliente_;}
+    
 
 };
 
